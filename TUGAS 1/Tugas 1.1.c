57ac902c64bfc9359cff9e72ad7401b37616f05f
AfrizaDicky <138385001+AfrizaDicky@users.noreply.github.com>
2024-09-09 21:08:20 +0700

    Update Tugas 1.1.c

---
+++ TUGAS 1/Tugas 1.1.c
@@ -0,0 +1,17 @@
+//Terdapat kesalahan pada kode tersebut, buatlah supaya kode tersebut tidak error ketika di run.
+//output = Masukan angka = (jika kurang dari 10 maka code akan selesai)
+//jika output lebih dari sepuluh maka akan keluar print yang ke 2
+
+
+#include <stdio.h>
+int angka;
+
+
+int main() {
+    printf("Masukan angka = ");
+    scanf(%d, &angka);
+    if(angka > 10){
+        printf("adalah angka anda %d", angka)
+        return 0;
+}
+
